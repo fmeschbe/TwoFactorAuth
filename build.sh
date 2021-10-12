b14fb80ab79ccca5786479c7aa3d0aa6021cf1b1
Felix Meschberger <felix@meschberger.ch>
2021-10-12 17:01:31 +0200

    Container to be called TwoFactorAuth
    Remove dependency on nginx version and properly label the
    container for what it contains: the TwoFactorAuth web app

--- build.sh
+++ build.sh
@@ -33,21 +33,12 @@ esac
 # Create Docker file for build from template
 sed "s/#{ARCH_TAG}/${ARCH_TAG}/g" Dockerfile-template > Dockerfile.${ARCH}
 
-DIST_IMAGE='alpine'
-DIST_TAG='3.14'
-DIST_REPO='http://dl-cdn.alpinelinux.org/alpine/v3.14/main/'
-DIST_REPO_PACKAGES='nginx'
-
-BUILD_VERSION_ARG="NGINX_VERSION"
-EXT_RELEASE=$(curl -sL "${DIST_REPO}x86_64/APKINDEX.tar.gz" |
-    tar -xz -C /tmp && awk '/^P:'"${DIST_REPO_PACKAGES}"'$/,/V:/' /tmp/APKINDEX |
-    sed -n 2p | sed 's/^V://'
-)
+COMMIT_SHA=$(git rev-parse --short HEAD)
+EXT_RELEASE="0.1-${COMMIT_SHA}"
 EXT_RELEASE_CLEAN=$(echo ${EXT_RELEASE} | sed 's/[~,%@+;:/]//g')
 VERSION_TAG="${EXT_RELEASE_CLEAN}-fme-1"
-COMMIT_SHA=$(git rev-parse HEAD)
 GITHUB_DATE=$(date '+%Y-%m-%dT%H:%M:%S%:z')
-IMAGE="fmeschbe/nginx"
+IMAGE="fmeschbe/twofactorauth"
 META_TAG="${VERSION_TAG}"
 
 docker build \
@@ -61,14 +52,13 @@ docker build \
    --label "org.opencontainers.image.vendor=meschberger.ch" \
    --label "org.opencontainers.image.licenses=GPL-3.0-only" \
    --label "org.opencontainers.image.ref.name=${COMMIT_SHA}" \
-   --label "org.opencontainers.image.title=Nginx" \
-   --label "org.opencontainers.image.description=[Nginx](https://nginx.org/) is a simple webserver with php support. The config files reside in \`/config\` for easy user customization." \
+   --label "org.opencontainers.image.title=TwoFactorAuth" \
+   --label "org.opencontainers.image.description=TwoFactorAuth is a web authentication portal providing two factor authentication(*2FA*)." \
    --no-cache \
    --pull \
    -f Dockerfile.${ARCH} \
    -t ${IMAGE}:${ARCH_TAG}-${META_TAG} \
    -t ${IMAGE}:latest \
-   --build-arg ${BUILD_VERSION_ARG}=${EXT_RELEASE} \
    --build-arg VERSION=\"${VERSION_TAG}\" \
    --build-arg BUILD_DATE=${GITHUB_DATE} .
 
